440651251611a42599f92a1067c18e3db4465a5b
nisha4r <nisha.4r@gmail.com>
2023-07-16 19:29:04 -0600

    Add logic for putDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('putDb call');
+
+  const connectiondb = await openDB('jate', 1);
+  const objStore = connectiondb.transaction('jate', 'readwrite').objectStore('jate');
+  const output = await objStore.put({id: 1, value: content});
+  console.log('Saved to the database', output);
+}; 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
