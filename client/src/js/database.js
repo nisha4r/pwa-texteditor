34dc2d0d3a375d0d23a98e5465e9a30f51f36fe3
nisha4r <nisha.4r@gmail.com>
2023-07-16 19:33:44 -0600

    add get db logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,11 +18,21 @@ export const putDb = async (content) => {
 
   const connectiondb = await openDB('jate', 1);
   const objStore = connectiondb.transaction('jate', 'readwrite').objectStore('jate');
-  const output = await objStore.put({id: 1, value: content});
+  const output = await objStore.put({ id: 1, value: content });
   console.log('Saved to the database', output);
-}; 
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('getDb call');
+  const connectiondb = await openDB('jate', 1);
+  const objStore = connectiondb.transaction('jate', 'readonly').objectStore('jate');
+  const output = await objStore.getAll();
+  console.log('get all result', output);
+  if(!output){
+    return output.value;
+  }
+  return output;
+}
 
 initdb();
