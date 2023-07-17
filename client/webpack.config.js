c8f2e9eb7cf1b4cef62aec91910ed03837c6cfcd
nisha4r <nisha.4r@gmail.com>
2023-07-16 19:59:47 -0600

    Add WebpackPWAManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,28 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE'
       }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        inject: true,
+        name: 'Just Another Text Editor (JATE)',
+        short_name: 'JATE',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
